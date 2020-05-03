c839ad3d9bcfc030ef5d28f549b0e034ce695626
Pony_v_tampone <pony.v.tampone@gmail.com>
2020-05-04 01:38:37 +0300

    Map Creator

---
+++ src/levels/Map/Map.c
@@ -0,0 +1,70 @@
+//
+// Created by Иван on 03.05.2020.
+//
+#include "game.h"
+
+#include "minilib.h"
+
+
+
+TextureMap *Map(SDL_Renderer *renderer, int arr[20][25])
+{
+    TextureMap* text = malloc(sizeof(TextureMap*));
+    text -> wallDown = LoadTexture("../resource/tiles/wall_mid.png", renderer);
+    text -> wallUp = LoadTexture("../resource/tiles/wall_top_left.png", renderer);
+    text -> wallLeft = LoadTexture("../resource/tiles/wall_side_mid_left.png", renderer);
+    text -> wallRight = LoadTexture("../resource/tiles/wall_side_mid_right.png", renderer);
+
+    //LOAD MAP
+    for (int row = 0; row < 20; row++)
+    {
+        for(int column = 0; column < 25; column++)
+        {
+            text -> map[row][column] = arr[row][column];
+        }
+    }
+
+    text -> src.x = text -> src.y = 0;
+
+    text -> dest.x = text -> dest.y = 0;
+
+    text -> src.w = text -> dest.w = text -> src.h = text -> dest.h = 32;
+
+    return text;
+
+}
+
+
+void DrawMap(SDL_Renderer *renderer, TextureMap *text)
+{
+    int type = 0;
+
+    for (int row = 0; row < 20; row++)
+    {
+        for(int column = 0; column < 25; column++)
+        {
+            type = text -> map[row][column];
+
+            text -> dest.x = column * 32;
+            text -> dest.y = row * 32;
+
+            switch (type)
+            {
+                case 1:
+                    SDL_RenderCopy(renderer, text -> wallDown, &(text -> src), &(text -> dest));
+                    break;
+                case 2:
+                    SDL_RenderCopy(renderer, text -> wallUp, &(text -> src), &(text -> dest));
+                    break;
+                case 3:
+                    SDL_RenderCopy(renderer, text -> wallLeft, &(text -> src), &(text -> dest));
+                    break;
+                case 4:
+                    SDL_RenderCopy(renderer, text -> wallRight, &(text -> src), &(text -> dest));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
