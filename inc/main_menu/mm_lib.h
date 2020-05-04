045dfe8dbfb11749e17764e194850f7f805f812e
FightForDobro <fightfordobro@gmail.com>
2020-05-04 11:18:50 +0300

    Main Menu

---
+++ inc/main_menu/mm_lib.h
@@ -0,0 +1,10 @@
+#ifndef EXALTEDJUPITER_DEV_MM_LIB_H
+#define EXALTEDJUPITER_DEV_MM_LIB_H
+
+#include "game.h"
+#include "minilib.h"
+
+/* Prototypes */
+void ProcessKeyBoard(SDL_Event event);
+
+#endif // EXALTEDJUPITER_DEV_MM_LIB_H
