a7f08759de6fb3f78bc6b1ac5695abb88e758957
Danyil Ivanov <divanov@student.ucode.world>
2020-05-04 17:00:12 +0300

    Fix itoa and mingw in linux

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,6 +1,8 @@
 #include "game.h"
 #include "minilib.h"
 
+char* mx_itoa(int value, char* str, int radix);
+
 SDL_Texture *AnimateByFrames(t_frameanim *tFrameanim)
 {
     /* Base Variables */
@@ -11,7 +13,7 @@ SDL_Texture *AnimateByFrames(t_frameanim *tFrameanim)
 
     /* Main Logic */
     frame_str = strcat(frame_str, tFrameanim->frames_dir);
-    frame_str = strcat(frame_str, itoa(frame, frame_count, 10));
+    frame_str = strcat(frame_str, mx_itoa(frame, frame_count, 10));
     frame_str = strcat(frame_str, ".png");
 
     /* Textures */
@@ -42,4 +44,29 @@ t_texture_ss LoadSpriteSheet(SDL_Texture *texture, SDL_Rect textureRect, SDL_Rec
     textureSs.frame = frameRect;
 
     return textureSs;
+}
+
+char* mx_itoa(int value, char* str, int radix) {
+    static char dig[] =
+            "0123456789"
+            "abcdefghijklmnopqrstuvwxyz";
+    int n = 0, neg = 0;
+    unsigned int v;
+    char* p, *q;
+    char c;
+    if (radix == 10 && value < 0) {
+        value = -value;
+        neg = 1;
+    }
+    v = value;
+    do {
+        str[n++] = dig[v%radix];
+        v /= radix;
+    } while (v);
+    if (neg)
+        str[n++] = '-';
+    str[n] = '\0';
+    for (p = str, q = p + n/2; p != q; ++p, --q)
+        c = *p, *p = *q, *q = c;
+    return str;
 }
