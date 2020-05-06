0db5b061099751d8fd35f09c85f1d100e33dfd5d
Rodion Prischepa <rodionp49@gmail.com>
2020-05-06 18:09:26 +0300

    damage simulation

--- src/enemies/enemy_do_damage.c
+++ src/enemies/enemy_do_damage.c
@@ -6,6 +6,5 @@ void enemy_do_damage(t_mainHero *mainHero, t_enemy enemy){
     mainHero->invulnerability_timer = SDL_GetTicks();
     if(mainHero->health <= 0)
         mainHero->alive = false;
-    SDL_Log("health %d", mainHero->health);
 }
 
